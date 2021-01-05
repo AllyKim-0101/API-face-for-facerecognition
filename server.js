b6850dd789f47c02c1e19ca7fedbcc7a6d13c98c
Ally Kim <57968966+AllyKim-0101@users.noreply.github.com>
2021-01-06 10:06:14 +1100

    updating database

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: "pg",
     connection: {
-        host: "127.0.0.1",
+        host: "postgresql-lively-90395",
         user: "postgres",
         password: "test",
         database: "smart-brain"
