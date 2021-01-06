6c27b6872b90c973d00d59d26a5068d8b650fc19
Ally Kim <57968966+AllyKim-0101@users.noreply.github.com>
2021-01-06 11:07:23 +1100

    update connection

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const image = require('./controllers/image');
 //connect postgres
 const db = knex({
     client: "pg",
-    connectionString: {
-        host: process.env.DATABASE_URL,
+    connection: {
+        connectionString: process.env.DATABASE_URL,
         ssl: {
             rejectUnauthorized: false
         }
